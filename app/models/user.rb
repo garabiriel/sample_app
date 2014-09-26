ca70bf2ab111078c7a23825f10db958ad858adc4
skyhub <gabriel@skyhub.com.br>
2014-09-26 18:26:42 -0300

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, confirmation:true, length: { minimum: 6 }
+  validates :password, presence: true, confirmation:true, length: { minimum: 6 }
+  after_validation { self.errors.messages.delete(:password_digest) }
 end
