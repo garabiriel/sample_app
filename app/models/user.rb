f503130a8797b001b0d5173fbc70759c37a2eaf7
skyhub <gabriel@skyhub.com.br>
2014-09-29 12:19:24 -0300

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,27 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   before_save { self.email = email.downcase }
+  before_create :create_remember_token
+
+ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, confirmation:true, length: { minimum: 6 }
   after_validation { self.errors.messages.delete(:password_digest) }
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.digest(User.new_remember_token)
+    end
 end
